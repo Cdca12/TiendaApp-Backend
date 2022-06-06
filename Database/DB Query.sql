83310959b1fb30c2b5aa3153aaca5d92684dad9f
Carlos Contreras <cdca12mx@gmail.com>
2022-06-05 22:17:15 -0600

    Created db query

---
+++ Database/DB Query.sql
@@ -0,0 +1,58 @@
+-- Create database
+
+USE master
+GO
+CREATE DATABASE TiendaApp;
+GO
+
+USE TiendaApp;
+GO
+
+-- Create tables
+
+-- Productos
+CREATE TABLE Product (
+  ProductID int PRIMARY KEY IDENTITY,
+  ProductName varchar(255),
+  ProductPrice int
+)
+GO
+
+CREATE TABLE Category (
+  CategoryID int PRIMARY KEY IDENTITY,
+  CategoryName varchar(255)
+)
+GO
+
+-- CategoriasProductos
+CREATE TABLE ProductCategory (
+  ProductID int FOREIGN KEY REFERENCES Product,
+  CategoryID int FOREIGN KEY REFERENCES Category,
+  PRIMARY KEY (ProductID, CategoryID)
+)
+GO
+
+CREATE TABLE Client (
+  ClientID int PRIMARY KEY,
+  ClientName varchar(255)
+)
+GO
+
+-- Pedidos
+CREATE TABLE [Order] (
+  OrderID int PRIMARY KEY IDENTITY,
+  OrderDate datetime,
+  OrderTotal decimal(19,4),
+  ClientID int FOREIGN KEY REFERENCES Client
+)
+GO
+
+CREATE TABLE OrderDetail (
+  OrderID int FOREIGN KEY REFERENCES [Order],
+  ProductID int FOREIGN KEY REFERENCES Product,
+  OrderQuantity int,
+  OrderProductPrice int,
+  PRIMARY KEY (OrderID, ProductID)
+)
+
+
